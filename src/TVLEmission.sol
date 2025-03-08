14f488b31c18c5d8768271a55fa9faa28c4799c1
catena <catena@protonmail.com>
2025-03-08 16:30:54 +0400

    mint tokens on response

--- src/TVLEmission.sol
+++ src/TVLEmission.sol
@@ -4,21 +4,45 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "quex-v1-interfaces/interfaces/core/IQuexActionRegistry.sol";
 import "./ParametricToken.sol";
 
+//TODO move to SDK?
 address constant QUEX_CORE = 0xD8a37e96117816D43949e72B90F73061A868b387;
 IQuexActionRegistry constant quexCore = IQuexActionRegistry(QUEX_CORE);
 
-contract TVLEmission is Ownable {
-    uint256 requestId;
-    parametricToken parametricToken;
+struct FlowResponse {
+    uint256 timestamp;
+    uint256 currentTVL;
+}
 
-    constructor() Ownable(msg.sender) {
+
+contract TVLEmission is Ownable {
+    address private _treasury;
+    uint256 private _requestId;
+    FlowResponse lastUpdate;
+    ParametricToken parametricToken;
+
+    constructor(address treasuryAddress) Ownable(msg.sender) {
         parametricToken = new ParametricToken();
+        _treasury = treasuryAddress;
     }
 
     // We will track the requests performed by the unique request Id assigned by Quex
     // Only keep the latest request Id
-    function request(uint256 flowId) public payable onlyOwner returns(uint256) {
-        requestId = quexCore.createRequest{value:msg.value}(flowId);
-        return requestId;
+    function request(uint256 flowId) public payable onlyOwner returns (uint256) {
+        _requestId = quexCore.createRequest{value: msg.value}(flowId);
+        return _requestId;
+    }
+
+    // Callback handling the data processing logic
+    function processResponse(uint256 receivedRequestId, DataItem memory response, IdType idType) external {
+        // Verify that the sender is indeed Quex
+        require(msg.sender == QUEX_CORE, "Only Quex Proxy can push data");
+        // Verify that the request was initiated on-chain, rather than off-chain
+        require(idType == IdType.RequestId, "Return type mismatch");
+        // Verify that the response corresponds to our request
+        require(receivedRequestId == _requestId, "Unknown request ID");
+        // Use the data. Decode and mint tokens
+        lastUpdate = abi.decode(response.value, (FlowResponse));
+        parametricToken.mint(_treasury, lastUpdate.currentTVL);
+        return;
     }
 }
